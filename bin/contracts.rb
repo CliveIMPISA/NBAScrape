2be9709d7e7e2f69e907f1ad08ea5592cb8ec278
Amon <amonmoce@yahoo.fr>
2014-10-18 22:46:41 +0800

    I replaced the original file by what I was trying to do

--- bin/contracts.rb
+++ bin/contracts.rb
@@ -3,11 +3,13 @@ fail ArgumentError, "Usage: ruby contracts.rb [teamname]\n
 Please check the readme file for team names abbreviation\n" if ARGV.count == 0
 
 team_name = ARGV[0]
-require_relative '../lib/salaryscrape.rb'
+require_relative '../lib/salaryscrape'
 include SalaryScraper
 
 code = BasketballReference.new
 
-# code.get_team(team_name)
-
-puts code.to_yaml(team_name)
+if(code.check_if_team_exist(team_name))
+	puts code.to_yaml(team_name)
+else 
+	print "This team does not exist.\nPlease check the readme file for team names abbreviation.\n"
+end
--- lib/salaryscrape.rb
+++ lib/salaryscrape.rb
@@ -8,7 +8,6 @@ module SalaryScraper
     WEBSITE = 'http://www.basketball-reference.com/contracts'
     PAYROLL = "//div[@id='div_payroll']//tbody//tr[@class='']//td"
     HEAD = "//div[@id='div_payroll']//thead//tr[@class='']//th"
-
     def get_team(team)
       doc = get_page(team)
       doc.xpath(PAYROLL)
@@ -29,18 +28,23 @@ module SalaryScraper
       doc = get_page(team)
       doc.xpath(HEAD)
     end
+    
+    def head_array(team)
+      @head_array = []
+      head = get_column_heads(team)
+      head.each { |x| @head_array << x.text }
+      @head_array
+    end
+
+    def players_data_array(team)
+      @players_data_array = []
+      players_data = get_team(team)
+      players_data.each { |x| @players_data_array << x.text }
+      @players_data_array
+    end
 
     def to_array_of_hashes(team)
-      players_data_array = []
-      head_array = []
-
-      players_data = get_team(team)
-      head = get_column_heads(team)
-
-      players_data.each { |x| players_data_array << x.text }
-      head.each { |x| head_array << x.text }
-      number_arrays = get_number_arrays(players_data_array, head_array)
-      assign_key_to_value(number_arrays, players_data_array, head_array)
+      assign_key_to_value(get_number_arrays, players_data_array(team), head_array(team))
     end
 
     def assign_key_to_value(number_arrays, players_data_array, head_array)
@@ -56,13 +60,21 @@ module SalaryScraper
       data
     end
 
-    def get_number_arrays(val1, val2)
-      val1.size / val2.size
+    def get_number_arrays
+      @players_data_array.size / @head_array.size
     end
 
     def to_yaml(team_name)
       data = to_array_of_hashes(team_name)
       data.to_yaml
+    end
+
+    def check_if_team_exist(team)
+      if (head_array(team).size == 0 || players_data_array(team).size == 0)
+        return false
+      else 
+        return true
+      end
     end
   end
 end
